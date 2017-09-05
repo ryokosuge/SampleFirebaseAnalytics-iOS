a127a7a6740ee8bff4f8f9d55dfea4f5e6aed3b3
ryokosuge <ryo.kosuge@gmail.com>
2017-09-05 17:30:31 +0900

    first commit.

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'SampleFirebaseAnalytics' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for SampleFirebaseAnalytics
+   # Firebase for iOS
+   pod 'Firebase/Core', '~> 4.1.1'
+   pod 'Firebase/Analytics', '~> 4.1.1'
+
+end
---
+++ SampleFirebaseAnalytics/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  SampleFirebaseAnalytics
+//
+//  Created by kosuge on 2017/09/05.
+//  Copyright © 2017年 RyoKosuge. All rights reserved.
+//
+
+import UIKit
+import FirebaseAnalytics
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var button: UIButton?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func tappedButton(_ button: UIButton) {
+        Analytics.logEvent("button_tap", parameters: nil)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
